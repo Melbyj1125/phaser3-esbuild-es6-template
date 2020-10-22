bf54820bcfdc340783995a300a104d2f9ccc7ff8
Seth Berrier <berriers@uwstout.edu>
2020-10-22 14:46:51 -0500

    Formatting and comments

--- src/sprites/Witch.js
+++ src/sprites/Witch.js
@@ -1,21 +1,27 @@
 import Phaser from 'phaser'
 import CONFIG from '../config.js'
 
+// Witch sprite object that adds itself to the given scene
 class WitchSprite extends Phaser.GameObjects.Sprite {
   constructor (scene, x, y) {
+    // Pass parameters to parent's constructor
     super(scene, x, y, 'witch', 1)
 
+    // Initialize animation info if it hasn't been already
     if (!WitchSprite.animInitialized) {
       WitchSprite.setupAnim(scene)
     }
 
+    // Add self to the given scene
     scene.add.existing(this)
   }
 }
 
+// Sprite animation configuration as static members
+// of the WitchSprite class
 WitchSprite.animInitialized = false
 WitchSprite.setupAnim = (scene) => {
-  console.log('Creating witch animation')
+  // Create the animations using the global anim controller
   scene.anims.create({
     key: 'witchWalkDown',
     frameRate: 10,
@@ -36,7 +42,10 @@ WitchSprite.setupAnim = (scene) => {
     repeat: -1,
     frames: scene.anims.generateFrameNumbers('witch', { start: 16, end: 23 })
   })
+
+  // Indicate that the animation has been setup
   WitchSprite.animInitialized = true
 }
 
+// Export class for access in other classes
 export default WitchSprite
